e6dc38f7dd3342a002a4e3a1987d2bbc710610a3
GunaVarma11 <gunavarmagv@gmail.com>
2024-02-25 14:53:24 +0530

    add

---
+++ school data/Controllers/StudentController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using  school_data.Models;
+
+public class StudentController : Controller
+{
+    public ActionResult Index()
+    {
+        return View();
+    }
+
+
+    [HttpPost]
+    public ActionResult Index(Student model)
+    {
+        if (ModelState.IsValid)
+        {
+
+            return RedirectToAction("Submitted", model);
+        }
+        else
+        {
+
+            return View(model);
+        }
+    }
+
+    public ActionResult Submitted(Student model)
+    {
+        return View(model);
+    }
+
+}
---
+++ school data/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace school_data.Models
+{
+    public class Student
+    {
+        
+            public string? Name { get; set; }
+            public int Age { get; set; }
+            public string? Email { get; set; }
+            public string? Course { get; set; }
+
+        }
+    }
